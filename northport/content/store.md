3716f5b0b89184a16c6c0960debadcb3b39b9416
monsieurverde <domverdemgmt@gmail.com>
2020-01-18 20:19:20 -0500

    testing shopping cart

---
+++ northport/content/store.md
@@ -0,0 +1,7 @@
++++
+slug = "store"
+description = "blog"
++++
+{{<cart>}}
+
+
---
+++ northport/static/shop/precache-manifest.15d2f50c211310ae9fc9ef8cae48b854.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "2995d79a12f2b95cd2315d66c5bb5084",
+    "url": "/shop/static/media/12064273040195392_1.2995d79a.jpg"
+  },
+  {
+    "revision": "e7de06b5c3d6d782d03e",
+    "url": "/shop/static/css/main.dead4997.chunk.css"
+  },
+  {
+    "revision": "663e6069f8f729ce765a",
+    "url": "/shop/static/js/1.663e6069.chunk.js"
+  },
+  {
+    "revision": "3a4c97e913adeac98b80",
+    "url": "/shop/static/js/runtime~main.3a4c97e9.js"
+  },
+  {
+    "revision": "b5714b6acfbff7904a24ea78662b9345",
+    "url": "/shop/static/media/100_1.b5714b6a.jpg"
+  },
+  {
+    "revision": "b618b5fc19f0517b8428bc9cae806df0",
+    "url": "/shop/static/media/101_1.b618b5fc.jpg"
+  },
+  {
+    "revision": "2e62289b2a88630ff3461c81af1756f7",
+    "url": "/shop/static/media/10412368723880252_1.2e62289b.jpg"
+  },
+  {
+    "revision": "54fbf816a3acf53727a2536d97d76881",
+    "url": "/shop/static/media/10547961582846888_1.54fbf816.jpg"
+  },
+  {
+    "revision": "0d941b4cc1da99273f35b2777b500ff3",
+    "url": "/shop/static/media/10686354557628304_1.0d941b4c.jpg"
+  },
+  {
+    "revision": "84c82a5a8f51a94e64024f67e8445d37",
+    "url": "/shop/static/media/11033926921508488_1.84c82a5a.jpg"
+  },
+  {
+    "revision": "06ec2c628b4a51a539881f0ee02b4e60",
+    "url": "/shop/static/media/11600983276356164_1.06ec2c62.jpg"
+  },
+  {
+    "revision": "7faf6a31b8557166976235d3bd9a8e2b",
+    "url": "/shop/static/media/11854078013954528_1.7faf6a31.jpg"
+  },
+  {
+    "revision": "e7de06b5c3d6d782d03e",
+    "url": "/shop/static/js/main.e7de06b5.chunk.js"
+  },
+  {
+    "revision": "d78e27906d034c3b4cd53aa3526acda3",
+    "url": "/shop/static/media/18532669286405344_1.d78e2790.jpg"
+  },
+  {
+    "revision": "75f51cecfca671162c53bdeed3cf549f",
+    "url": "/shop/static/media/18644119330491310_1.75f51cec.jpg"
+  },
+  {
+    "revision": "4c799bac69deb57ceba00bcdf15e6ec2",
+    "url": "/shop/static/media/27250082398145996_1.4c799bac.jpg"
+  },
+  {
+    "revision": "a313534ce6b430f29143cafee1eb5a3e",
+    "url": "/shop/static/media/39876704341265610_1.a313534c.jpg"
+  },
+  {
+    "revision": "df947f142806310944c26d76bf1b8fa0",
+    "url": "/shop/static/media/51498472915966370_1.df947f14.jpg"
+  },
+  {
+    "revision": "843a471381aada57cdd38f66662ab1af",
+    "url": "/shop/static/media/5619496040738316_1.843a4713.jpg"
+  },
+  {
+    "revision": "45a051e062934c657ca67819884cbf23",
+    "url": "/shop/static/media/6090484789343891_1.45a051e0.jpg"
+  },
+  {
+    "revision": "87b5353e208124cb9acc7cadd973a3bb",
+    "url": "/shop/static/media/8552515751438644_1.87b5353e.jpg"
+  },
+  {
+    "revision": "df97d7c2d0fc5e5d9346b660ce3936fa",
+    "url": "/shop/static/media/876661122392077_1.df97d7c2.jpg"
+  },
+  {
+    "revision": "59b643658d74a090ed326573a46e07a7",
+    "url": "/shop/static/media/9197907543445676_1.59b64365.jpg"
+  },
+  {
+    "revision": "9b698d6dbfaf10d84d983440e2b5a433",
+    "url": "/shop/static/media/100_2.9b698d6d.jpg"
+  },
+  {
+    "revision": "4a6cafc5e3c64543199da740329ac1a2",
+    "url": "/shop/index.html"
+  }
+];
